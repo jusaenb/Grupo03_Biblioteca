612e8d2829f2ce2b7e69dac2dbfbcaf7a2a56928
enamezto <enamezto@CCTL132-10>
2025-11-12 11:51:17 +0100

    MD Personal

---
+++ MD/Personal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD
+{
+    public abstract class Personal
+    {
+        public string Dni { get; set; }
+        public string Nombre { get; }
+        public Rol Rol { get; protected set; }
+        /// <summary>
+        /// Inicializa una nueva instancia de clase Personal.
+        /// </summary>
+        /// <param name="dni">Número de identificación único del trabajador</param>
+        /// <param name="nombre">Nombre del trabajador</param>
+        public Personal(string dni, string nombre)
+        {
+            Dni = dni;
+            Nombre = nombre;
+        }
+    }
+}
