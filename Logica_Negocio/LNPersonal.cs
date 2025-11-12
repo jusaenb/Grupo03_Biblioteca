249cd8c908b7e0e55240e52304eb04f888b411f2
enamezto <enamezto@CCTL132-10>
2025-11-12 11:53:36 +0100

    LN Personal

---
+++ Logica_Negocio/LNPersonal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MD;
+
+namespace LN
+{
+    public class LNPersonal
+    {
+        public Personal Personal { get; private set; }
+        public LNPersonal(Personal personal)
+        {
+            this.Personal = personal;
+        }
+        public bool Loguearse(string password)
+        {
+            return true;
+        }
+        public void darAltaUsuario(string dni, string nombre)
+        {
+        }
+        public void darBajaUsuario(string dni, string nombre)
+        {
+        }
+    }
+}
---
+++ Logica_Negocio/LNPersonalAdquisiciones.cs
@@ -0,0 +1,30 @@
+﻿using MD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LN
+{
+    public class LNPersonalAdquisiciones : LNPersonal
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="personalAdquisiciones"></param>
+        public LNPersonalAdquisiciones(PersonalAdquisiciones personalAdquisiciones) : base(personalAdquisiciones)
+        {
+        }
+        /// <summary>
+        /// ghgh
+        /// </summary>
+        /// <param name="isbn"></param>
+        /// <param name="titulo"></param>
+        public void DarAltaDocumento(string isbn, string titulo)
+        {
+
+        }
+
+    }
+}
