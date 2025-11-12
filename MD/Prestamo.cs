daa796558124164884f6b03e98f970da5fcfa86e
jusaenb <jusaenb@CCTL132-25>
2025-11-12 11:03:57 +0100

    prestamo

---
+++ MD/Prestamo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD
+{
+    internal class Prestamo
+    {
+        private int idPrestamo;
+        private DateTime FechaInicio;
+        private DateTime FechaDevolucionMaxima;
+        private DateTime FechaDevolucionReal;
+        private String estadoPrestamo;
+
+        public Prestamo()
+        {
+            //idPrestamo = 
+            FechaInicio = DateTime.Now;
+            if (//prestamo de libros en papel)
+            {
+                FechaDevolucionMaxima = FechaInicio.AddDays(15);
+            }
+            else
+            {
+                FechaDevolucionMaxima = FechaInicio.AddDays(10);
+            }
+            estadoPrestamo = "En Proceso";
+        }
+
+        public void finalizarPrestamo()
+        {
+            FechaDevolucionReal = DateTime.Now;
+            estadoPrestamo = "Finalizado";
+        }
+
+        public int diasRetrasoDevolucion()
+        {
+            if (FechaDevolucionReal > FechaDevolucionMaxima)
+            {
+                TimeSpan retraso = FechaDevolucionReal - FechaDevolucionMaxima;
+                return retraso.Days;
+            }
+            return 0;
+        }
+
+    }
+}
