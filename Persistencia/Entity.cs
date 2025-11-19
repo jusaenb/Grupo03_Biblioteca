cc90302dc46f3cc28b14d9f5085a986307954760
paramila <paramila@CCTL132-09>
2025-11-19 10:48:49 +0100

    clase enttity

---
+++ Persistencia/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    internal class Entity<T>
+    {
+        private T id;
+        public Entity(T id)
+        {
+            this.id = id;
+        }
+        public T Id
+        {
+            get
+            {
+                return this.id;
+            }
+        }
+    }
+}
--- Persistencia/Tabla.cs
+++ Persistencia/Tabla.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Persistencia
 {
-    public class Tabla<
+    public class Tabla
     {
     }
 }
