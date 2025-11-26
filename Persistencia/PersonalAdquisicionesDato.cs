dd5d8caf462a4b626ef57daac227175e59527b84
enamezto <enamezto@CCTL132-11>
2025-11-26 10:16:11 +0100

    personal

---
+++ Persistencia/PersonalAdquisicionesDato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    internal class PersonalAdquisicionesDato : PersonalDato
+    {
+        public PersonalAdquisicionesDato(String dni, String nombre) : base(dni, nombre)
+        {
+        }
+    }
+}
---
+++ Persistencia/PersonalDato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    internal class PersonalDato : Entity<String>
+    {
+        private String nombre;
+
+        public PersonalDato(String dni, String nombre) : base(dni)
+        {
+            this.nombre = nombre;
+        }
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+}
