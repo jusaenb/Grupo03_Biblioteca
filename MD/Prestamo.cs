1d3dad2ac3ece0dd2ddada177fca53445cb9238c
enamezto <126153376+enamezto@users.noreply.github.com>
2025-11-26 11:42:36 +0100

    prestamo

--- MD/Prestamo.cs
+++ MD/Prestamo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,34 +9,36 @@ namespace MD
     public class Prestamo
     {
         private Usuario usuario;
-        private Ejemplar ejemplar;
+        private List<Ejemplar> ejemplares;
         private DateTime fechaPrestamo;
         private DateTime fechaDevolucion;
         private string estado; 
-        public Prestamo(Usuario usuario, Ejemplar ejemplar, DateTime fechaPrestamo, string estado)
+        public Prestamo(Usuario usuario, List<Ejemplar> ejemplares, DateTime fechaPrestamo, string estado)
         {
             this.usuario = usuario;
-            this.ejemplar = ejemplar;
+            this.ejemplares = ejemplares ?? new List<Ejemplar>();
             this.fechaPrestamo = fechaPrestamo;
-           if(ejemplar.Documento is AudioLibro)
+            bool contieneAudiolibro = this.ejemplares.Any(e => e.Documento is AudioLibro);
+
+            if (contieneAudiolibro)
             {
-                this.fechaDevolucion = fechaPrestamo.AddDays(10); // 7 días para audiolibros
+                this.fechaDevolucion = fechaPrestamo.AddDays(10);
             }
             else
             {
-                this.fechaDevolucion = fechaPrestamo.AddDays(15); // 14 días para otros documentos
+                this.fechaDevolucion = fechaPrestamo.AddDays(15);
             }
-            this.estado = estado;
         }
         public Usuario Usuario
         {
             get { return usuario; }
             set { usuario = value; }
         }
-        public Ejemplar Ejemplar
+        public List<Ejemplar> Ejemplares
         {
-            get { return ejemplar; }
-            set { ejemplar = value; }
+            get { return ejemplares; }
+
+            set { ejemplares = value; }
         }
         public DateTime FechaPrestamo
         {
